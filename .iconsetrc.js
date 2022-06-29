527dd5eb98e6b7a7b99aaa4ea13e79dcbd232050
Fábio Calliari <fabio.calliari@nutrien.com>
2022-06-29 14:15:10 -0300

    feat(icon-launcher): add icon-set-creator and corresponding assets

---
+++ .iconsetrc.js
@@ -0,0 +1,7 @@
+// .iconsetrc.js
+module.exports = {
+  imagePath: './assets/images/ic_launcher.png',
+
+  adaptiveIconBackground: './assets/images/ic_background.png',
+  adaptiveIconForeground: './assets/images/ic_foreground.png',
+};
